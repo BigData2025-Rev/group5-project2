491008c7fdcbd50029823ddb653c558fcd7175e0
Jarrod <xjsaechao@gmail.com>
2025-01-16 12:34:11 -0800

    Added casting for price and fixed null bug issue for trend

--- src/data_generator.py
+++ src/data_generator.py
@@ -7,6 +7,7 @@ from pyspark.sql import SparkSession
 from pyspark.sql import functions as fun
 from pyspark.sql.window import Window
 from pyspark.sql.types import StringType
+from pyspark.sql.types import DoubleType
 
 spark = SparkSession.builder.appName("data_generator").getOrCreate()
 
@@ -249,6 +250,9 @@ def generate_final_data(random_seed, date_start : datetime.datetime, date_end : 
 
     all_columns = ["order_id"] + [col for col in orders.columns if col != "order_id"]
     orders = orders.select(*all_columns)
+
+    orders = orders.withColumn("price", fun.regexp_replace(orders["price"], r"[^0-9.]", ""))
+    orders = orders.withColumn("price", orders["price"].cast(DoubleType()))
     return orders
 
 #Selects up to 5% of the orders to convert to rogue data
@@ -272,7 +276,7 @@ def add_seasonal_trend(orders):
     orders = orders.withColumn("month", fun.month("datetime"))
     orders = orders.withColumn(
         "price",
-        fun.when(fun.col("month").isin(2,4,6,8,10,12),fun.col("price") * 0.75)
+        fun.when(fun.col("month").isin(1,3,5,7,9,11),fun.col("price") * 0.75)
         .otherwise(fun.col("price"))
     )
 
@@ -289,7 +293,8 @@ date_end = datetime.datetime(2024, 12, 31)
 orders = generate_final_data(random_seed, date_start, date_end)
 #orders = add_rogue_data(random_seed, orders)
 orders = orders.orderBy("order_id")
-orders = add_seasonal_trend(orders)
+
+# orders = add_seasonal_trend(orders)
 orders.show()
 print(orders.count())
 
